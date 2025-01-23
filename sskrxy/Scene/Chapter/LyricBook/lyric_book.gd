92ca012cdf17ef4e6c94de3fc4748ee3186e4e13
lxdua <2753792469@QQ.COM>
2025-01-23 14:56:58 +0800

    捡东西

--- sskrxy/Scene/Chapter/LyricBook/lyric_book.gd
+++ sskrxy/Scene/Chapter/LyricBook/lyric_book.gd
@@ -3,9 +3,14 @@ extends Node2D
 
 @onready var texture_button: TextureButton = $TextureButton
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var panel_container: PanelContainer = $CanvasLayer/PanelContainer
 
-func read_it():
-	pass
 
 func _on_texture_button_pressed() -> void:
-	pass
+	animation_player.play("fade in")
+	await animation_player.animation_finished
+	panel_container.show()
+
+func _on_panel_container_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("mouse_left"):
+		panel_container.hide()
