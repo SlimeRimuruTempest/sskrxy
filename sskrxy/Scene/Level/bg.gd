82caeb77df4ad420ffc1bcb8fe1f2713532cdc19
lxdua <2753792469@QQ.COM>
2025-02-10 02:35:52 +0800

    补刀

--- sskrxy/Scene/Level/bg.gd
+++ sskrxy/Scene/Level/bg.gd
@@ -7,6 +7,8 @@ extends Sprite2D
 var tot_time: float
 
 func _physics_process(delta: float) -> void:
+	if update_time.is_empty():
+		return
 	tot_time += delta
 	if update_time[0] <= tot_time:
 		update_time.pop_front()
