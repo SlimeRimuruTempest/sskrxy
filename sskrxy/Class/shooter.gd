23bf437008886d6240eeae4cd13dee58d58854c5
lxdua <2753792469@QQ.COM>
2025-02-09 21:20:36 +0800

    子弹池

--- sskrxy/Class/shooter.gd
+++ sskrxy/Class/shooter.gd
@@ -8,8 +8,17 @@ class_name Shooter
 ## 发射完到销毁的等待时间
 @export var end_time: float
 
+
 ## 发射物
-@export var bullet_ps: PackedScene = null
+@export var bullet_pool: Array[PackedScene]
+
+enum ShootMode1 { PICK_SEQUENCE, PICK_RANDOM }
+enum ShootMode2 { EVERY_MOMENT, EVERY_BULLET }
+
+## 从子弹池顺序抽取还是随机抽取
+@export var shoot_mode_1: ShootMode1
+## 抽取是对于每轮齐射还是每颗子弹
+@export var shoot_mode_2: ShootMode2
 
 ## 发射物速率曲线
 @export var bullet_lin_curve: Curve = Curve.new()
@@ -33,11 +42,32 @@ class_name Shooter
 ## 是否跟随老妈
 @export var bullet_follow_mum: bool = false
 
+var pool_id: int = 0:
+	set(v):
+		pool_id = v % bullet_pool.size()
+
+func get_bullet() -> PackedScene:
+	var bullet_ps: PackedScene = null
+	match shoot_mode_1:
+		ShootMode1.PICK_SEQUENCE:
+			bullet_ps = bullet_pool[pool_id]
+			pool_id += 1
+		ShootMode1.PICK_RANDOM:
+			bullet_ps = bullet_pool.pick_random()
+	return bullet_ps
+
 func shoot_once():
+	var bullet_ps: PackedScene = null
 	for i in once_num:
+		match shoot_mode_2:
+			ShootMode2.EVERY_MOMENT:
+				if bullet_ps == null:
+					bullet_ps = get_bullet()
+			ShootMode2.EVERY_BULLET:
+				bullet_ps = get_bullet()
 		#var lin_off: = once_lin_curve.sample(i)
+		var bullet: = bullet_ps.instantiate()
 		var ang_off: = once_ang_curve.sample_baked(i)
-		var bullet: NodeSTG = bullet_ps.instantiate()
 		if bullet_follow_mum:
 			add_child(bullet)
 		else:
