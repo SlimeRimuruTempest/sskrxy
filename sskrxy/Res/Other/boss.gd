153b408922d1cd6bdf05bbe8e630b199d7e5bb1a
SlimeRimuruTempest <y2971139282@outlook.com>
2025-02-11 03:15:28 +0800

    finished

--- sskrxy/Res/Other/boss.gd
+++ sskrxy/Res/Other/boss.gd
@@ -6,12 +6,3 @@ extends Sprite2D
 @export var wait_time: float
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var timer: Timer = $Timer
-
-func _ready() -> void:
-	hide()
-	timer.start(wait_time)
-
-func _on_timer_timeout() -> void:
-	show()
-	animation_player.play("move")
