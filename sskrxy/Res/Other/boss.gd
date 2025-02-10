b5e2a2fe09d3f1b61e6977b1e313841d7fa2ece9
SlimeRimuruTempest <y2971139282@outlook.com>
2025-02-10 23:04:46 +0800

    0

--- sskrxy/Res/Other/boss.gd
+++ sskrxy/Res/Other/boss.gd
@@ -6,14 +6,5 @@ extends NodeSTG
 
 
 func _physics_process(delta: float) -> void:
-	super(delta)
-	if abs(lin) <= 10:
-		sprite.texture = sm
-		return
-	var dir: Vector2 = get_dir(ang)
-	if dir.x < 0:
-		sprite.texture = sl
-	elif dir.x > 0:
-		sprite.texture = sr
-	else:
-		sprite.texture = sm
+	return
+	
