af1581c8a3a77f53aa9fe58eb576fa562172ed1c
lxdua <2753792469@QQ.COM>
2025-02-10 16:04:23 +0800

    血条闪烁 子弹加了是否为一次性的开关（是不是碰玩家了就消失

--- sskrxy/Scene/Player/health_bar.gd
+++ sskrxy/Scene/Player/health_bar.gd
@@ -1,5 +1,7 @@
 extends TextureProgressBar
 
+@export var animation_player: AnimationPlayer
 
 func _on_player_health_updated(current_health: float, max_health: float) -> void:
 	value = current_health / max_health
+	animation_player.play("blink")
