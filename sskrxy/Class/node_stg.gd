9afc4a60a1d0f45b5faacb029d8bf0ba4fcf9520
SlimeRimuruTempest <y2971139282@outlook.com>
2025-02-10 19:42:27 +0800

    覆盖文件

--- sskrxy/Class/node_stg.gd
+++ sskrxy/Class/node_stg.gd
@@ -2,6 +2,12 @@ extends Node2D
 class_name NodeSTG
 
 @onready var marker: = get_tree().get_first_node_in_group("Marker")
+@onready var sprite: Sprite2D = $Sprite
+
+## 素材的旋转偏移
+@export var sprite_rot_off: float = 0.0
+## 是否锁住sprite旋转
+@export var lock_rot: bool = false
 
 ## 以多少度角为0度角
 @export var ori_ang: float
@@ -17,14 +23,24 @@ var tot_time: float = 0
 
 var lin: float:
 	get:
-		return lin_curve.sample_baked(tot_time)
+		return lin_curve.sample(tot_time)
 var ang: float:
 	get:
-		return ang_curve.sample_baked(tot_time) + ori_ang
+		return ang_curve.sample(tot_time) + ori_ang
 func get_dir(a: float):
 	return Vector2.from_angle(deg_to_rad(a))
 func get_movement(delta: float):
 	return get_dir(ang) * lin * delta
+
+func _ready() -> void:
+	if not lock_rot:
+		sprite.rotation_degrees = ang + sprite_rot_off
+	else:
+		sprite.rotation_degrees = sprite_rot_off
+
+func _process(delta: float) -> void:
+	if not lock_rot:
+		sprite.rotation_degrees = ang + sprite_rot_off
 
 func _physics_process(delta: float) -> void:
 	if visible == false:
